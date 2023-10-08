175228165cb13a5e10c8a51b36abdb1bc9d0c61b
mehwishjbe <mehwish.jbe@gmail.com>
2023-10-08 12:50:29 +0200

    delete button

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ const inputField = document.getElementById('input-field');
 const listedItems = document.getElementById('listed-items');
 
 addButton.addEventListener('click', function(){
-    const div = document.createElement('div');
-    listedItems.appendChild(div);
-    div.innerText = inputField.value;
-    console.log(listedItems);
-    inputField.value = "";
+    if(inputField.value === ""){
+        alert('enter text');
+    }else{
+        const div = document.createElement('div');
+        listedItems.appendChild(div);
+
+        const textField = document.createElement('span')
+        textField.innerText = inputField.value;
+        div.appendChild(textField);
+
+        console.log(listedItems);
+        inputField.value = "";
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+
+    deleteButton.addEventListener('click', function(){
+        listedItems.removeChild(div);
+    });
+    div.appendChild(deleteButton);
+    
+    
+    }
 });
+
+   
